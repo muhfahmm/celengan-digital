d0476711f019aa5c3b395912ff6580374328214a
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2026-01-23 19:51:15 +0700

    celengan

--- mobile/constants/Config.ts
+++ mobile/constants/Config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://192.168.1.1/celengan-digital/web'; // Ganti dengan IP komputer Anda
+export const API_URL = 'http://192.168.0.105/celengan-digital/web'; // IP otomatis terdeteksi
 export const AUTH_API = `${API_URL}/auth/api`;
 export const DATA_API = `${API_URL}/data-celengan/api`;
 export const TRANSACTION_API = `${API_URL}/transaksi/api`;
