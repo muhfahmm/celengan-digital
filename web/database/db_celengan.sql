184eb101b620f3f7aca1386149f1a3a5701e50af
Fahim <fahimfahim0407@gmail.com>
2026-05-31 23:13:48 +0700

    Make users email column nullable in schema

--- web/database/db_celengan.sql
+++ web/database/db_celengan.sql
@@ -317,7 +317,7 @@ DELIMITER ;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `email` varchar(150) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
